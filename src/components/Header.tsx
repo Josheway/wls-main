ab90e5dba4982d904724b850438170f0e5b2e693
Josh Williams <josheway@gmail.com>
2021-10-10 00:14:48 -0500

    Update Header.tsx
    added menu items and routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,20 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { Link as RouterLink } from 'react-router-dom';
 
 export default function ButtonAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -19,6 +30,7 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
@@ -30,6 +42,19 @@ export default function ButtonAppBar() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose} component={RouterLink} to={'/Other'}>Other</MenuItem>
+        <MenuItem onClick={handleClose} component={RouterLink} to={'/products'}>Products</MenuItem>
+        <MenuItem onClick={handleClose}>Login</MenuItem>
+      </Menu>
     </Box>
   );
 }
