3089bb0f7878bb35fabc51f63bbeff9602fda54e
Josh <josheway@gmail.com>
2021-11-24 15:01:26 -0600

    Update Other.tsx

--- src/pages/Other.tsx
+++ src/pages/Other.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, FC } from "react";
+import React, { ReactElement, FC, useState, useEffect } from "react";
 
 
 
@@ -13,11 +13,38 @@ interface Props {
 
 const Other: FC<Props> = ({ title }): ReactElement => {
 
+    const [data, setData] = useState([]);
+
+    const apiGet = () => {
+        fetch('https://knv7atr5oe.execute-api.us-east-1.amazonaws.com/default/fetchData')
+        .then((response) => response.json())
+        .then((json) => {
+            console.log (json);
+            setData(json);
+        });
+    };
+
+    useEffect(() => {
+        apiGet();
+        
+    }, []);
+
     return (
 
         <div>
 
             {title}
+
+            {/* <button onClick={apiGet} >Fetch API</button> */}
+            <br />
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+
+            <div>
+                <ul>
+                    {data.map( (item: any) => (
+                        <li key={item.index} >{item.shortDesc}</li> ))};
+                </ul>
+            </div>
 
         </div>
 
